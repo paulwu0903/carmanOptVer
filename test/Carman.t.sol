b1ecd69e8caffef6f232faf7ea789b02475f18e7
Paul Wu <paulwu0903@gmail.com>
2023-04-18 10:30:25 +0800

    Update: append Merkle Tree for whitelist

---
+++ test/Carman.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/CarMan.sol";
+
+contract CarmanTest is Test {
+    CarMan public carman;
+
+    function setUp() public {
+        
+    }
+
+}
